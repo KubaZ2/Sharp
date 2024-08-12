208fef315223b65b3087a9867762767fe301311b
KubaZ2 <jazloty@o2.pl>
2024-08-12 16:30:33 +0200

    Allow multiple `JitGenericAttribute`s

--- Sharp.Runtime/JitGenericAttribute.cs
+++ Sharp.Runtime/JitGenericAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sharp;
 
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = true)]
 public sealed class JitGenericAttribute(params Type[] types) : Attribute
 {
     public IReadOnlyList<Type> Types { get; } = types;
